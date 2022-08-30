a93e06f4a1b654c46dc29ad5bad4bce7a4e61d59
d-ava <d.avalishvili@gmail.com>
2022-08-30 18:32:14 +0400

    status bar color changed. search bar.
    next step. room.

--- app/src/main/java/com/example/breakingbadcompose/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/breakingbadcompose/ui/search/SearchScreen.kt
@@ -5,10 +5,13 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.breakingbadcompose.ui.login.BBTextField
 import com.example.breakingbadcompose.ui.theme.bb_active_color
 import com.example.breakingbadcompose.ui.theme.bb_background
 import com.example.breakingbadcompose.ui.uiComponents.BBProgressIndicator
@@ -18,22 +21,18 @@ import java.nio.file.Files.size
 @Composable
 fun SearchScreen(){
 
+    val searchTextState = remember{ mutableStateOf("")}
+
     Box(
         modifier = androidx.compose.ui.Modifier
             .background(color = bb_background)
-            .fillMaxSize(), contentAlignment = Alignment.Center
+            .fillMaxSize(), contentAlignment = Alignment.TopCenter
     ) {
 
-        Text(
-            fontSize = MaterialTheme.typography.h3.fontSize,
-            fontWeight = FontWeight.Bold,
-            text = "Search Screen",
-            color = bb_active_color
-        )
+Column() {
 
-
-
-
+    BBTextField(text = searchTextState, title ="search" )
+}
 
     }
 
